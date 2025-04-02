217a650ef683c67cd71cb27c638e2db5c575d905
pradyumnac26 <pradyumnac.ec17@rvce.edu.in>
2025-04-01 22:31:12 -0600

    parkinglot

---
+++ parkinglot/vehicletype/Car.java
@@ -0,0 +1,6 @@
+package parkinglot.vehicletype;
+public class Car extends Vehicle {
+    public Car(String licensePlate) {
+        super(licensePlate, VehicleType.CAR);
+    }
+}
---
+++ parkinglot/vehicletype/Motorcycle.java
@@ -0,0 +1,6 @@
+package parkinglot.vehicletype;
+public class Motorcycle extends Vehicle {
+    public Motorcycle(String licensePlate) {
+        super(licensePlate, VehicleType.MOTORCYCLE);
+    }
+}
